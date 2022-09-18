71529d41fb29f19d8e2d068371f3cbf0f7c64ec6
Beverly <beverlydahunsi@gmail.com>
2022-09-18 06:02:41 -0700

    lowercase and uppercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+int al;
+
+for (al = 'a'; al <= 'z'; al++)
+{
+putchar (al);
+}
+for (al = 'A'; al <= 'Z'; al++)
+{
+putchar (al);
+}
+putchar('\n');
+return (0);
+}
